c2b54c603dcbca47ca887c4b63d0e679c0e415bf
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-11-09 09:53:24 +0800

    fix caching

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const redis = require("redis")
+const redis = require("redis");
 const dotenv = require("dotenv");
 const people = require("./data.json");
 const Person = require("./model/person");
@@ -29,11 +29,15 @@ app.get("/", (req, res) => {
 
 app.get("/people", async (req, res) => {
   try {
-    const foundPeople = await client.get('people') || await Person.find();
-    const numFound = foundPeople.length;
-    client.set("people", numFound);
-    res.status(200).json(numFound);
-  } catch(err) {
+    let data = await client.get("people");
+    if (data === null) {
+      const foundPeople = await Person.find();
+      data = foundPeople.length;
+      client.set("people", data);
+    }
+    const numFound = +data;
+    return res.status(200).json(numFound);
+  } catch (err) {
     return res.sendStatus(500);
   }
 });
@@ -58,7 +62,17 @@ app.delete("/people", async (req, res) => {
     });
 });
 
+app.post("/flush", async (req, res) => {
+  try {
+    await client.flushAll();
+    return res.sendStatus(200);
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+});
+
 app.listen(3000, async () => {
   await client.connect();
+  await client.flushAll();
   console.log("server listening on port 3000");
 });
